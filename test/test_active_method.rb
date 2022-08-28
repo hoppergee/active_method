761c85a997be02feaab6d37ec09feddbd3d15641
Hopper Gee <hopper.gee@hey.com>
2022-08-28 15:16:54 +0800

    Fix a test error

--- test/test_active_method.rb
+++ test/test_active_method.rb
@@ -2,12 +2,10 @@
 
 require "test_helper"
 
-class TestActiveMethod < Minitest::Test
+class TestActiveMethod < ApplicationTest 
+
   def test_that_it_has_a_version_number
     refute_nil ::ActiveMethod::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
