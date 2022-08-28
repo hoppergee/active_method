a45f56c22fa19adeb8d51bcdc6113510b7ce5e13
Hopper Gee <hopper.gee@hey.com>
2022-08-28 15:13:33 +0800

    Install simplecov

--- Gemfile
+++ Gemfile
@@ -11,3 +11,4 @@ gem "minitest", "~> 5.0"
 
 gem "debug"
 gem "matrixeval-ruby"
+gem "simplecov"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter "/test"
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "active_method"
 
 require "minitest/autorun"
+require "debug"
